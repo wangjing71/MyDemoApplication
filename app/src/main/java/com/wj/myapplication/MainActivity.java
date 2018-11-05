6ef63e4177c2391662f55e278055503a56e7d3d5
WangJing <397383523@qq.com>
2018-11-05 17:51:13 +0800

    删除无用代码，基本功能完成

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import myproj.wj.com.myapplication.R;
+import dsfsdf.wj.sdfsf.myapplication.R;
+
 
 public class MainActivity extends BaseActivity {
 
@@ -36,6 +40,8 @@ public class MainActivity extends BaseActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(intent);
 
             }
         });
