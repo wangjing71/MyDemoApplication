14f303026d11b21bb3f2958c3f4fd677893d34bd
397383523@qq.com <8362781wj>
2019-01-02 12:11:00 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
@@ -41,6 +43,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void doSomeThing() {
-
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+        startActivity(intent);
     }
 }
