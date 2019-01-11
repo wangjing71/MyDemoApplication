b1dcb2bca1cb116919ff0845b4da342e12ea9e72
397383523@qq.com <8362781wj>
2019-01-11 16:46:26 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.gyf.barlibrary.ImmersionBar;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.RefreshState;
+import com.scwang.smartrefresh.layout.listener.OnMultiPurposeListener;
 
 
 public class MainActivity extends BaseActivity {
@@ -18,6 +24,7 @@ public class MainActivity extends BaseActivity {
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
     public MyAdapter businessSelectAdapter;
+    private SmartRefreshLayout smartRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,7 @@ public class MainActivity extends BaseActivity {
         recyclerView = findViewById(R.id.recyclerView);
         toolBars.getBackground().setAlpha(0);
         statbarHeight.getBackground().setAlpha(0);
+        smartRefreshLayout = findViewById(R.id.refreshLayout);
     }
 
     @Override
@@ -63,7 +71,6 @@ public class MainActivity extends BaseActivity {
                 View fitstView = linearLayoutManager.findViewByPosition(0);
                 if (fitstView != null) {
                     int dis = (int) Math.abs(fitstView.getY());
-                    Log.i("====", dis + "");
                     if (dis > 1000) {
                         dis = 1000;
                     }
@@ -71,6 +78,69 @@ public class MainActivity extends BaseActivity {
                     statbarHeight.getBackground().setAlpha(255 * dis / 1000);
                     ImmersionBar.setStatusBarView(MainActivity.this, statbarHeight);
                 }
+            }
+        });
+
+        smartRefreshLayout.setOnRefreshLoadMoreListener(new OnMultiPurposeListener() {
+            @Override
+            public void onHeaderMoving(RefreshHeader header, boolean isDragging, float percent, int offset, int headerHeight, int maxDragHeight) {
+
+            }
+
+            @Override
+            public void onHeaderReleased(RefreshHeader header, int headerHeight, int maxDragHeight) {
+
+            }
+
+            @Override
+            public void onHeaderStartAnimator(RefreshHeader header, int headerHeight, int maxDragHeight) {
+
+            }
+
+            @Override
+            public void onHeaderFinish(RefreshHeader header, boolean success) {
+
+            }
+
+            @Override
+            public void onFooterMoving(RefreshFooter footer, boolean isDragging, float percent, int offset, int footerHeight, int maxDragHeight) {
+
+            }
+
+            @Override
+            public void onFooterReleased(RefreshFooter footer, int footerHeight, int maxDragHeight) {
+
+            }
+
+            @Override
+            public void onFooterStartAnimator(RefreshFooter footer, int footerHeight, int maxDragHeight) {
+
+            }
+
+            @Override
+            public void onFooterFinish(RefreshFooter footer, boolean success) {
+
+            }
+
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+
+            }
+
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+
+            }
+
+            @Override
+            public void onStateChanged(@NonNull RefreshLayout refreshLayout, @NonNull RefreshState oldState, @NonNull RefreshState newState) {
+               if(newState == RefreshState.None){
+                   Log.i("====","1111");
+                   toolBars.setVisibility(View.VISIBLE);
+               }else{
+                   Log.i("====","2222");
+                   toolBars.setVisibility(View.GONE);
+               }
             }
         });
     }
