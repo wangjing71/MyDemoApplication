9e6a56b8f121565d01977720877e948c366e3b05
397383523@qq.com <8362781wj>
2019-01-11 15:31:20 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.gyf.barlibrary.ImmersionBar;
 
@@ -10,6 +12,8 @@ import com.gyf.barlibrary.ImmersionBar;
 public class MainActivity extends BaseActivity {
 
     private View statbarHeight;
+    private LinearLayout toolBars;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,8 @@ public class MainActivity extends BaseActivity {
     protected void initView() {
         statbarHeight = findViewById(R.id.statbarheight);
         ImmersionBar.setStatusBarView(this,statbarHeight);
+        toolBars = findViewById(R.id.toolbars);
+        recyclerView = findViewById(R.id.recyclerView);
     }
 
     @Override
