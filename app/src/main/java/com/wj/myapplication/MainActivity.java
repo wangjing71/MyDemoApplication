b91a4925b19018eff1386875447586ffbe7e7c2b
397383523@qq.com <8362781wj>
2019-01-11 16:22:21 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -15,7 +19,19 @@ public class MainActivity extends BaseActivity {
     private View statbarHeight;
     private LinearLayout toolBars;
     private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
     public MyAdapter businessSelectAdapter;
+    private int index = 0;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            index ++;
+            toolBars.getBackground().setAlpha(index);
+            statbarHeight.getBackground().setAlpha(index);
+            handler.sendEmptyMessageDelayed(0,200);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +46,18 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         statbarHeight = findViewById(R.id.statbarheight);
-        ImmersionBar.setStatusBarView(this,statbarHeight);
+        ImmersionBar.setStatusBarView(this, statbarHeight);
         toolBars = findViewById(R.id.toolbars);
         recyclerView = findViewById(R.id.recyclerView);
+        toolBars.getBackground().setAlpha(0);
+        statbarHeight.getBackground().setAlpha(0);
+        handler.sendEmptyMessage(0);
     }
 
     @Override
     protected void initData() {
         businessSelectAdapter = new MyAdapter(this);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setNestedScrollingEnabled(false);
         recyclerView.setItemAnimator(null);
@@ -47,6 +66,26 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
+//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//            }
+//
+//            @Override
+//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//                View fitstView = linearLayoutManager.findViewByPosition(0);
+//                if (fitstView != null) {
+//                    int dis = (int) Math.abs(fitstView.getY());
+//                    Log.i("====", dis + "");
+//                    if (dis > 1000) {
+//                        dis = 1000;
+//                    }
+//                    toolBars.getBackground().setAlpha(255 * dis / 1000);
+//                }
+//            }
+//        });
     }
 
     private void doSomeThing() {
