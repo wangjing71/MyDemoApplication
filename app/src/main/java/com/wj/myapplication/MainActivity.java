1e1093b3f81264c93338a4221cd702f2f8530b86
397383523@qq.com <8362781wj>
2019-01-14 15:39:26 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,22 @@
 package com.wj.myapplication;
 
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 public class MainActivity extends BaseActivity {
 
     private TextView wenzi;
+    private RelativeLayout parent;
+
+    private boolean mBackEnable = false;
+    private boolean mIsBtnBack = false;
+    private int rootBottom = Integer.MIN_VALUE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         wenzi = findViewById(R.id.wenzi);
+        parent = findViewById(R.id.parent);
     }
 
     @Override
@@ -31,6 +41,26 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
+        parent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                parent.getGlobalVisibleRect(r);
+                // 进入Activity时会布局，第一次调用onGlobalLayout，先记录开始软键盘没有弹出时底部的位置
+                if (rootBottom == Integer.MIN_VALUE) {
+                    rootBottom = r.bottom;
+                    return;
+                }
+                // adjustResize，软键盘弹出后高度会变小
+                if (r.bottom < rootBottom) {
+                    mBackEnable = false;
+                    Log.i("====","1111");
+                } else {
+                    Log.i("====","2222");
+                    mBackEnable = true;
+                }
+            }
+        });
     }
 
 }
