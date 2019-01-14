2b08defe508a6449471e26da120cca78380430f1
397383523@qq.com <8362781wj>
2019-01-14 15:43:27 +0800

    1

--- app/src/main/java/com/wj/myapplication/BaseActivity.java
+++ app/src/main/java/com/wj/myapplication/BaseActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.gyf.barlibrary.ImmersionBar;
+
 
 /**
  * Created by Administrator on 2018/1/4.
@@ -16,6 +18,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutId());
+        ImmersionBar.with(this).init();
         initView();
         initData();
         setEvent();
