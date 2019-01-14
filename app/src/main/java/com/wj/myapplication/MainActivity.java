4879ce7221562b54e5dcc8725b7105019a01e6bb
397383523@qq.com <8362781wj>
2019-01-14 16:26:37 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,11 +1,17 @@
 package com.wj.myapplication;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends BaseActivity {
 
+    private RelativeLayout parent;
+    private int keyHeight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        parent = findViewById(R.id.parent);
+        keyHeight = getWindowManager().getDefaultDisplay().getHeight() / 3;
     }
 
     @Override
@@ -31,4 +38,18 @@ public class MainActivity extends BaseActivity {
     protected void setEvent() {
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        parent.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (oldBottom != 0 && bottom != 0 && (oldBottom - bottom > keyHeight)) {
+                    Log.i("====", "1111");
+                } else if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > keyHeight)) {
+                    Log.i("====", "2222");
+                }
+            }
+        });
+    }
 }
