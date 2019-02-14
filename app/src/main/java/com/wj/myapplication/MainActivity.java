564dc7e190a820f71bb60093ed19dd34dacfd1bf
397383523@qq.com <8362781wj>
2019-02-14 17:28:15 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.wj.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends BaseActivity {
 
     private Button button;
+    private TextView textView;
+    private LocalBroadcastManager localBroadcastManager;
+    private MyBroadcastReceiver myBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,12 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("hello");
+        myBroadcastReceiver = new MyBroadcastReceiver();
+        localBroadcastManager.registerReceiver(myBroadcastReceiver,intentFilter);
     }
 
     @Override
@@ -41,6 +57,21 @@ public class MainActivity extends BaseActivity {
     }
 
     private void doSomeThing() {
+        localBroadcastManager.sendBroadcast(new Intent("hello"));
+    }
 
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        localBroadcastManager.unregisterReceiver(myBroadcastReceiver);
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show();
+            textView.setText("111");
+        }
     }
 }
