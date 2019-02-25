94431409888e2176a94a89b277afbdfca6ee2f61
397383523@qq.com <8362781wj>
2019-02-25 12:50:42 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -2,10 +2,22 @@ package com.wj.myapplication;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeOption;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 import com.tbruyelle.rxpermissions2.Permission;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
@@ -14,7 +26,9 @@ import io.reactivex.functions.Consumer;
 
 public class MainActivity extends BaseActivity {
 
-    private Button button;
+    private Button button,button2;
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +44,22 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
     }
 
     @Override
     protected void initData() {
-
+        //百度地图 start
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setIsNeedAddress(true);
+        option.setIsNeedLocationPoiList(true);
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");
+        option.setOpenGps(true);
+        mLocationClient.setLocOption(option);
+        //百度地图 end
     }
 
     @Override
@@ -45,6 +70,42 @@ public class MainActivity extends BaseActivity {
                 doSomeThing();
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchAdd("上海市浦东新区华绣路255弄10号401");
+            }
+        });
+    }
+
+    private void searchAdd(String str) {
+        GeoCoder mCoder = GeoCoder.newInstance();
+        OnGetGeoCoderResultListener listener = new OnGetGeoCoderResultListener() {
+            @Override
+            public void onGetGeoCodeResult(GeoCodeResult geoCodeResult) {
+                if (null != geoCodeResult && null != geoCodeResult.getLocation()) {
+                    if (geoCodeResult == null || geoCodeResult.error != SearchResult.ERRORNO.NO_ERROR) {
+                        Toast.makeText(MainActivity.this, "没有检索到结果", Toast.LENGTH_SHORT).show();
+                        //没有检索到结果
+                        return;
+                    } else {
+                        double latitude = geoCodeResult.getLocation().latitude;
+                        double longitude = geoCodeResult.getLocation().longitude;
+                        Toast.makeText(MainActivity.this, latitude+"__"+longitude, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onGetReverseGeoCodeResult(ReverseGeoCodeResult reverseGeoCodeResult) {
+                
+            }
+        };
+        mCoder.setOnGetGeoCodeResultListener(listener);
+        mCoder.geocode(new GeoCodeOption()
+                .city("上海")
+                .address(str));
     }
 
     private void doSomeThing() {
@@ -65,8 +126,26 @@ public class MainActivity extends BaseActivity {
     }
 
     private void showLocation() {
-
-
-
+        mLocationClient.start();
     }
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取经纬度相关（常用）的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            Log.i("====", location.getAddrStr());
+            Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+            double latitude = location.getLatitude();    //获取纬度信息
+            double longitude = location.getLongitude();    //获取经度信息
+            float radius = location.getRadius();    //获取定位精度，默认值为0.0f
+            String coorType = location.getCoorType();
+            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+            int errorCode = location.getLocType();
+            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+            mLocationClient.stop();
+        }
+    }
+
 }
--- app/src/main/java/com/wj/myapplication/MyApplication.java
+++ app/src/main/java/com/wj/myapplication/MyApplication.java
@@ -2,6 +2,7 @@ package com.wj.myapplication;
 
 import android.app.Application;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.tencent.bugly.Bugly;
 
 /**
@@ -14,5 +15,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Bugly.init(getApplicationContext(), "9c59c60539", false);
+        SDKInitializer.initialize(this);
     }
 }
