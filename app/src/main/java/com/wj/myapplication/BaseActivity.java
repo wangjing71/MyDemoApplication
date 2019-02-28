ee0bf7cff7c7ce5725007e780416f214a3f411a1
397383523@qq.com <8362781wj>
2019-02-28 12:18:11 +0800

    1

--- app/src/main/java/com/wj/myapplication/BaseActivity.java
+++ app/src/main/java/com/wj/myapplication/BaseActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
 
 /**
  * Created by Administrator on 2018/1/4.
@@ -12,10 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class BaseActivity extends AppCompatActivity {
 
 
+    protected RxPermissions rxPermissions;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutId());
+        rxPermissions = new RxPermissions(this);
         initView();
         initData();
         setEvent();
