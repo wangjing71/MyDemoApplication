c61fcc907bca850fe831ff23492b8e1b650272bd
397383523@qq.com <8362781wj>
2019-02-28 13:51:22 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wj.myapplication;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -51,14 +54,7 @@ public class MainActivity extends BaseActivity {
     }
 
     private void doSomeThing() {
-        getAppList();
-        installAPk();
-    }
-
-    private void installAPk() {
-
-
-
+        open1("com.sh.cm.shydhn");
     }
 
     private void getAppList() {
@@ -72,6 +68,62 @@ public class MainActivity extends BaseActivity {
             } else {
                 // 系统应用
             }
+        }
+    }
+
+//    private void openApp(String pac){
+//        Intent intent = new Intent(Intent.ACTION_MAIN);
+//        ComponentName cn =new  ComponentName(packageName,className);
+//        intent.setComponent(cn);
+//        startActivity(intent);
+//    }
+
+    private void doStartApplicationWithPackageName(String packagename) {
+
+        // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = getPackageManager().getPackageInfo(packagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
+        }
+
+        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+
+        // 通过getPackageManager()的queryIntentActivities方法遍历
+        List<ResolveInfo> resolveinfoList = getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packagename = 参数packname
+            String packageName = resolveinfo.activityInfo.packageName;
+            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname]
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            // 设置ComponentName参数1:packagename参数2:MainActivity路径
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            startActivity(intent);
+        }
+    }
+
+    private void open1(String pac){
+        Intent intent = getPackageManager().getLaunchIntentForPackage(pac);
+        if (intent != null) {
+            intent.putExtra("type", "110");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
 }
