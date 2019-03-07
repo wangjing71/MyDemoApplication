b718d6e27330e0e6bf212090649a73d7e8fced5f
397383523@qq.com <8362781wj>
2019-03-07 11:25:39 +0800

    1

--- app/src/main/java/com/wj/myapplication/BaseActivity.java
+++ app/src/main/java/com/wj/myapplication/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.gyf.barlibrary.ImmersionBar;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 
@@ -20,6 +21,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutId());
+        ImmersionBar.with(this).init();
         rxPermissions = new RxPermissions(this);
         initView();
         initData();
@@ -30,4 +32,10 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected abstract void initView() ;
     protected abstract void initData() ;
     protected abstract void setEvent() ;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ImmersionBar.with(this).destroy();
+    }
 }
