ead292fc903cdb034760caf28d109bb347a08cc6
397383523@qq.com <8362781wj>
2019-03-07 11:37:34 +0800

    1

--- app/src/main/java/com/wj/myapplication/BaseActivity.java
+++ app/src/main/java/com/wj/myapplication/BaseActivity.java
@@ -3,8 +3,11 @@ package com.wj.myapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
 
 import com.gyf.barlibrary.ImmersionBar;
+import com.gyf.barlibrary.OnKeyboardListener;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 
@@ -21,7 +24,16 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutId());
-        ImmersionBar.with(this).init();
+        ImmersionBar.with(this)
+                .keyboardEnable(true)
+                .keyboardMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)  //单独指定软键盘模式
+                .setOnKeyboardListener(new OnKeyboardListener() {    //软键盘监听回调
+                    @Override
+                    public void onKeyboardChange(boolean isPopup, int keyboardHeight) {
+                        Log.i("====",isPopup+"___"+keyboardHeight);
+                    }
+                })
+                .init();  //必须调用方可沉浸式
         rxPermissions = new RxPermissions(this);
         initView();
         initData();
