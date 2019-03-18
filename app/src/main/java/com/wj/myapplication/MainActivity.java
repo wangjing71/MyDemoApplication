fe70bcea6568b05a9351be168f92f2ca72e1c0c5
397383523@qq.com <8362781wj>
2019-03-18 17:10:45 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.wj.jcvideo.JCUserAction;
+import com.wj.jcvideo.JCUserActionStandard;
 import com.wj.jcvideo.JCVideoPlayerStandard;
 
 
@@ -29,6 +32,7 @@ public class MainActivity extends BaseActivity {
     protected void initData() {
         jcVideoPlayerStandard.setUp("http://192.168.3.100:8080/aaa.mp4", JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "测试视频");
         jcVideoPlayerStandard.startPlayLogic();
+        jcVideoPlayerStandard.setJcUserAction(new MyUserActionStandard());
     }
 
     @Override
@@ -39,5 +43,62 @@ public class MainActivity extends BaseActivity {
     protected void onPause() {
         super.onPause();
         jcVideoPlayerStandard.releaseAllVideos();
+    }
+
+    class MyUserActionStandard implements JCUserActionStandard {
+
+        @Override
+        public void onEvent(int type, String url, int screen, Object... objects) {
+            switch (type) {
+                case JCUserAction.ON_CLICK_START_ICON:
+                    Log.i("USER_EVENT", "ON_CLICK_START_ICON" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_START_ERROR:
+                    Log.i("USER_EVENT", "ON_CLICK_START_ERROR" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_START_AUTO_COMPLETE:
+                    Log.i("USER_EVENT", "ON_CLICK_START_AUTO_COMPLETE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_PAUSE:
+                    Log.i("USER_EVENT", "ON_CLICK_PAUSE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_CLICK_RESUME:
+                    Log.i("USER_EVENT", "ON_CLICK_RESUME" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_SEEK_POSITION:
+                    Log.i("USER_EVENT", "ON_SEEK_POSITION" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_AUTO_COMPLETE:
+                    Log.i("USER_EVENT", "ON_AUTO_COMPLETE" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_ENTER_FULLSCREEN:
+                    Log.i("USER_EVENT", "ON_ENTER_FULLSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_QUIT_FULLSCREEN:
+                    Log.i("USER_EVENT", "ON_QUIT_FULLSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_ENTER_TINYSCREEN:
+                    Log.i("USER_EVENT", "ON_ENTER_TINYSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_QUIT_TINYSCREEN:
+                    Log.i("USER_EVENT", "ON_QUIT_TINYSCREEN" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_TOUCH_SCREEN_SEEK_VOLUME:
+                    Log.i("USER_EVENT", "ON_TOUCH_SCREEN_SEEK_VOLUME" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserAction.ON_TOUCH_SCREEN_SEEK_POSITION:
+                    Log.i("USER_EVENT", "ON_TOUCH_SCREEN_SEEK_POSITION" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserActionStandard.ON_CLICK_START_THUMB:
+                    Log.i("USER_EVENT", "ON_CLICK_START_THUMB" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                case JCUserActionStandard.ON_CLICK_BLANK:
+                    Log.i("USER_EVENT", "ON_CLICK_BLANK" + " title is : " + (objects.length == 0 ? "" : objects[0]) + " url is : " + url + " screen is : " + screen);
+                    break;
+                default:
+                    Log.i("USER_EVENT", "unknow");
+                    break;
+            }
+        }
     }
 }
