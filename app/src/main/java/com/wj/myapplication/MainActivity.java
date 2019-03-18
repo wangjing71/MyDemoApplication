e0776b04354bb9cd05ae48fa5382d20b041e2018
397383523@qq.com <8362781wj>
2019-03-18 16:54:13 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.wj.jcvideo.JCVideoPlayerStandard;
 
@@ -8,6 +12,19 @@ import com.wj.jcvideo.JCVideoPlayerStandard;
 public class MainActivity extends BaseActivity {
 
     private JCVideoPlayerStandard jcVideoPlayerStandard;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            int st = jcVideoPlayerStandard.getCurrentPositionWhenPlaying();
+            if(st>0){
+                Toast.makeText(MainActivity.this, "开始播放", Toast.LENGTH_SHORT).show();
+            }else{
+                handler.sendEmptyMessageDelayed(0,200);
+            }
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +43,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        handler.sendEmptyMessage(0);
         jcVideoPlayerStandard.setUp("http://192.168.3.100:8080/aaa.mp4", JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "测试视频");
-        jcVideoPlayerStandard.getDuration();
         jcVideoPlayerStandard.startPlayLogic();
     }
 
     @Override
     protected void setEvent() {
-    }
-
-    private void doSomeThing() {
-
     }
 }
