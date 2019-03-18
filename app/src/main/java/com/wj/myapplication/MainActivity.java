443bb53e57d8d3a26598e3c61806d943a0629d15
397383523@qq.com <8362781wj>
2019-03-18 16:11:52 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,12 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 
 public class MainActivity extends BaseActivity {
 
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +21,14 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        button = findViewById(R.id.button);
     }
 
     @Override
     protected void initData() {
-
     }
 
     @Override
     protected void setEvent() {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doSomeThing();
-            }
-        });
     }
 
     private void doSomeThing() {
