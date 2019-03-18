b8819ab45c46e92791b97beeef53762144201197
397383523@qq.com <8362781wj>
2019-03-18 16:25:07 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -26,7 +26,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-        jcVideoPlayerStandard.setUp("http://2449.vod.myqcloud.com/2449_22ca37a6ea9011e5acaaf51d105342e3.f20.mp4", JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "测试视频");
+        jcVideoPlayerStandard.setUp("http://192.168.3.100:8080/aaa.mp4", JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "测试视频");
+        jcVideoPlayerStandard.getDuration();
+        jcVideoPlayerStandard.startWindowFullscreen();
     }
 
     @Override
