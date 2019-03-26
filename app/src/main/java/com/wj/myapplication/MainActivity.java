f89205b142e566b1807af1b562157efbd19dc4df
397383523@qq.com <8362781wj>
2019-03-26 14:13:05 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends BaseActivity {
         try {
             // 打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
             writer = new FileWriter(fileName, true);
-            writer.write(content);
+            writer.write(content+"\r\n");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
