f0de8e3d4795b19f7a0f012cffc1f96503f8ad7a
397383523@qq.com <8362781wj>
2019-03-29 16:56:43 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,7 +28,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-
+        startActivity(new Intent(this,Main3Activity.class));
     }
 
     @Override
