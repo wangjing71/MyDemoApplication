c6068d4fb71ed2316f489c0a11e87a07f6479f7a
397383523@qq.com <8362781wj>
2019-04-18 16:43:05 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,10 +1,16 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.gyf.barlibrary.ImmersionBar;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.adapter.Call;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
 
 
 public class MainActivity extends BaseActivity {
@@ -66,11 +72,30 @@ public class MainActivity extends BaseActivity {
     }
 
     private void doPost() {
+        OkGo.<String>post("http://www.baidu.com")
+                .tag(this)
+                .cacheKey("cachePostKey")
+                .cacheMode(CacheMode.DEFAULT)
+                .params("param2", "paramValue2")
+                .params("param3", "paramValue3")
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
 
+                    }
+                });
     }
 
     private void doGet() {
+        OkGo.<String>get("http://www.baidu.com")                            // 请求方式和请求url
+                .tag(this)                       // 请求的 tag, 主要用于取消对应的请求
+                .cacheKey("cacheKey")            // 设置当前请求的缓存key,建议每个不同功能的请求设置一个
+                .cacheMode(CacheMode.DEFAULT)    // 缓存模式，详细请看缓存介绍
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
 
+                    }
+                });
     }
-
 }
