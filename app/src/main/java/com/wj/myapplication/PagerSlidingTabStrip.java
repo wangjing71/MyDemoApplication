52849455d0b7ebdf6cdd27018514c1ad6e45a13c
397383523@qq.com <8362781wj>
2019-04-28 17:51:02 +0800

    1

---
+++ app/src/main/java/com/wj/myapplication/PagerSlidingTabStrip.java
@@ -0,0 +1,24 @@
+package com.wj.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+/**
+ * author wangjing
+ * Date 2019/4/28
+ * Description
+ */
+public class PagerSlidingTabStrip extends HorizontalScrollView {
+    public PagerSlidingTabStrip(Context context) {
+        super(context);
+    }
+
+    public PagerSlidingTabStrip(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PagerSlidingTabStrip(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
