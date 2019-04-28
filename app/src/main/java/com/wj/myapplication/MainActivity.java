1d587e2a66cb8fd14f9111d0d9723166f338fca0
397383523@qq.com <8362781wj>
2019-04-28 18:03:34 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.wj.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 public class MainActivity extends BaseActivity {
     private PagerSlidingTabStrip myTab;
     private ViewPager mViewPager;
+    private ArrayList<String> titles;
     private ArrayList<Fragment> fragList;
 
     @Override
@@ -26,13 +29,43 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initData() {
         fragList = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            fragList.add(new ChildFragment());
+        titles = new ArrayList<>();
+
+        titles.add("基础套餐");
+        titles.add("自有业务");
+        titles.add("群组业务");
+        titles.add("其他业务");
+
+        for (int i = 0; i < 4; i++) {
+            ChildFragment childFragment = new ChildFragment();
+            fragList.add(childFragment);
         }
     }
 
     @Override
     protected void setEvent() {
+    }
+
+    class MyBusinessAdapter extends FragmentPagerAdapter {
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+
+        public MyBusinessAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragList.size();
+        }
     }
 
 }
