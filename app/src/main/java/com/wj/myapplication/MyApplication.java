cfc6546ce509373ad70d10a25e2c11abac4b45a3
397383523@qq.com <8362781wj>
2019-05-07 16:55:13 +0800

    1

--- app/src/main/java/com/wj/myapplication/MyApplication.java
+++ app/src/main/java/com/wj/myapplication/MyApplication.java
@@ -3,6 +3,7 @@ package com.wj.myapplication;
 import android.app.Application;
 
 import com.shuyu.gsyvideoplayer.player.PlayerFactory;
+import com.shuyu.gsyvideoplayer.utils.GSYVideoType;
 import com.tencent.bugly.Bugly;
 
 import tv.danmaku.ijk.media.exo2.Exo2PlayerManager;
@@ -17,6 +18,11 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Bugly.init(getApplicationContext(), "9c59c60539", false);
+        initGSYVideoPlayer();
+    }
+
+    private void initGSYVideoPlayer() {
         PlayerFactory.setPlayManager(Exo2PlayerManager.class);//EXO模式
+        GSYVideoType.setShowType(GSYVideoType.SCREEN_TYPE_FULL);
     }
 }
