fc809ea1c028923d1faefd4885785cc63f101e8b
397383523@qq.com <8362781wj>
2019-05-10 10:56:48 +0800

    1

--- app/src/main/java/com/wj/myapplication/MyRxPerMission.java
+++ app/src/main/java/com/wj/myapplication/MyRxPerMission.java
@@ -1,13 +1,28 @@
 package com.wj.myapplication;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 import android.util.Log;
 
+import com.tbruyelle.rxpermissions2.Permission;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.tbruyelle.rxpermissions2.RxPermissionsFragment;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.functions.Function;
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * author wangjing
@@ -27,9 +42,45 @@ public class MyRxPerMission extends RxPermissions {
         rxPermissions = new RxPermissions(fragment);
     }
 
-    @Override
+
+    public void setLogging(boolean logging) {
+        rxPermissions.setLogging(logging);
+    }
+
+    public <T> ObservableTransformer<T, Boolean> ensure(final String... permissions) {
+        return rxPermissions.ensure(permissions);
+    }
+
+    public <T> ObservableTransformer<T, Permission> ensureEach(final String... permissions) {
+        return rxPermissions.ensureEach(permissions);
+    }
+
+    public <T> ObservableTransformer<T, Permission> ensureEachCombined(final String... permissions) {
+        return rxPermissions.ensureEachCombined(permissions);
+    }
+
     public Observable<Boolean> request(String... permissions) {
-        Log.i("====","成功装饰");
         return rxPermissions.request(permissions);
+    }
+
+    public Observable<Permission> requestEach(String... permissions) {
+        return rxPermissions.requestEach(permissions);
+    }
+
+    public Observable<Permission> requestEachCombined(String... permissions) {
+        return rxPermissions.requestEachCombined(permissions);
+    }
+
+
+    public Observable<Boolean> shouldShowRequestPermissionRationale(Activity activity, String... permissions) {
+        return rxPermissions.shouldShowRequestPermissionRationale(activity,permissions);
+    }
+
+    public boolean isGranted(String permission) {
+        return rxPermissions.isGranted(permission);
+    }
+
+    public boolean isRevoked(String permission) {
+        return rxPermissions.isRevoked(permission);
     }
 }
