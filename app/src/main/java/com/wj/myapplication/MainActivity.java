ab737d34ea440ab6e28f5bef1eb1a081859215a1
397383523@qq.com <8362781wj>
2019-05-13 10:35:17 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,5 +1,10 @@
 package com.wj.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
 
@@ -9,6 +14,9 @@ import com.igexin.sdk.PushManager;
 public class MainActivity extends BaseActivity {
 
     private Button button;
+
+    private GTuiMessageBroadcastReceiver gTuiMessageBroadcastReceiver;
+    private LocalBroadcastManager localBroadcastManager;
 
     @Override
     protected int setLayoutId() {
@@ -24,6 +32,16 @@ public class MainActivity extends BaseActivity {
     protected void initData() {
         PushManager.getInstance().initialize(getApplicationContext(), DemoPushService .class);
         PushManager.getInstance().registerPushIntentService(getApplicationContext(), DemoIntentService .class);
+        //注册广播监听登录状态变化
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("login_state_change");
+        gTuiMessageBroadcastReceiver = new GTuiMessageBroadcastReceiver();
+        localBroadcastManager.registerReceiver(gTuiMessageBroadcastReceiver, intentFilter);
+
+
+
+
     }
 
     @Override
@@ -38,5 +56,14 @@ public class MainActivity extends BaseActivity {
 
     private void doSomeThing() {
 
+    }
+
+    public class GTuiMessageBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+
+
+        }
     }
 }
