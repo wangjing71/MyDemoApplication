8fce773823beeb42f64bb341c1e39a0217c220da
397383523@qq.com <8362781wj>
2019-05-21 12:33:33 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.kt
+++ app/src/main/java/com/wj/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gyf.barlibrary.ImmersionBar
 import com.wj.myapplication.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ImmersionBar.with(this).init()
     }
 }
