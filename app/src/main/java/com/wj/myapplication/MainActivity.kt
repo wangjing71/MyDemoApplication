ad18680427ac6b161d975bdff8a05bb238988b1b
397383523@qq.com <8362781wj>
2019-05-21 12:32:56 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.kt
+++ app/src/main/java/com/wj/myapplication/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.myapplication
 
-import com.wj.myapplication.BaseActivity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import com.wj.myapplication.R
 
-class MainActivity : BaseActivity() {
-    override fun setLayoutId(): Int {
-        return R.layout.activity_main;
-    }
+class MainActivity : AppCompatActivity() {
 
-    override fun initView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun initData() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun setEvent() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
