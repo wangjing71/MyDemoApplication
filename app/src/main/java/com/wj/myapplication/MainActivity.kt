185de1665a95eecd0444d6cd5b5b60ae5aa20f90
397383523@qq.com <8362781wj>
2019-05-21 13:36:54 +0800

    1

--- app/src/main/java/com/wj/myapplication/MainActivity.kt
+++ app/src/main/java/com/wj/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.wj.myapplication.BaseActivity
 import com.wj.myapplication.R
 
 class MainActivity : BaseActivity() {
+
     override fun setLayoutId(): Int {
         return R.layout.activity_main
     }
