b2d61dc2d450406ecb2c2f325da930f2706cf248
397383523@qq.com <8362781wj>
2019-07-18 16:13:21 +0800

    标准master

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
@@ -8,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends BaseActivity {
 
     private Button button;
-
+    public static final int PICK_CONTACT = 0x201;
     @Override
     protected int setLayoutId() {
         return R.layout.activity_main;
@@ -35,6 +37,8 @@ public class MainActivity extends BaseActivity {
     }
 
     private void doSomeThing() {
-
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
     }
 }
