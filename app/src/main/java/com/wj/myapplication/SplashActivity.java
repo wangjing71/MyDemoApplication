769996005d5535df78de07f9a287dba7299d9d21
397383523@qq.com <8362781wj>
2019-08-02 14:24:49 +0800

    启动方案1

--- app/src/main/java/com/wj/myapplication/SplashActivity.java
+++ app/src/main/java/com/wj/myapplication/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        ImmersionBar.with(this).init();
+//        ImmersionBar.with(this).init();
         initView();
         initData();
         setEvent();
