7a64398a0f92c8537ab972b1eded492bbc05a771
397383523@qq.com <8362781wj>
2019-08-02 14:20:35 +0800

    标准master

--- app/src/main/java/com/wj/myapplication/SplashActivity.java
+++ app/src/main/java/com/wj/myapplication/SplashActivity.java
@@ -2,18 +2,22 @@ package com.wj.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import com.gyf.barlibrary.ImmersionBar;
 
 /**
  * author wangjing
  * Date 2019/7/29
  * Description
  */
-public class SplashActivity extends FragmentActivity {
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImmersionBar.with(this).init();
         initView();
         initData();
         setEvent();
