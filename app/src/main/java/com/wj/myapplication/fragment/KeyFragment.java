93af15f94c2242d8b6ef695f49f85dae96743acc
397383523@qq.com <8362781wj>
2019-08-12 09:44:18 +0800

    启动方案1

--- app/src/main/java/com/wj/myapplication/fragment/KeyFragment.java
+++ app/src/main/java/com/wj/myapplication/fragment/KeyFragment.java
@@ -1,12 +1,13 @@
 package com.wj.myapplication.fragment;
 
-import android.content.Intent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.wj.myapplication.R;
 import com.wj.myapplication.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 
 /**
@@ -15,6 +16,15 @@ import com.wj.myapplication.base.BaseFragment;
  * Description 金库钥匙
  */
 public class KeyFragment extends BaseFragment {
+
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.button1)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.wangjing)
+    Button wangjing;
 
     //需要无参构造方法
     public KeyFragment() {
@@ -28,5 +38,19 @@ public class KeyFragment extends BaseFragment {
     @Override
     public void initData() {
 
+    }
+
+    @OnClick({R.id.button, R.id.button1, R.id.button2, R.id.wangjing})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                break;
+            case R.id.button1:
+                break;
+            case R.id.button2:
+                break;
+            case R.id.wangjing:
+                break;
+        }
     }
 }
