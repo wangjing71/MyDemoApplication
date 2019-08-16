e6287042bf3a5950edc0acd06a59248ed9da4350
397383523@qq.com <8362781wj>
2019-08-16 14:16:19 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,23 +1,20 @@
 package com.wj.myapplication;
 
-import android.view.View;
+import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.wj.myapplication.base.BaseActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.button)
-    Button button;
+
     @BindView(R.id.button1)
     Button button1;
-    @BindView(R.id.button2)
-    Button button2;
 
     @Override
     protected int setLayoutId() {
@@ -29,18 +26,4 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.button, R.id.button1, R.id.button2})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                Toast.makeText(this, "111", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button1:
-                Toast.makeText(this, "111", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button2:
-                Toast.makeText(this, "111", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
 }
