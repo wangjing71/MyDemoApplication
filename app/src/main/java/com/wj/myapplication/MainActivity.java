91dee2d59f2b7dd9e57fe9eceb8e36976f705474
397383523@qq.com <8362781wj>
2019-08-16 14:18:10 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,7 @@
 package com.wj.myapplication;
 
-import android.os.Bundle;
-import android.widget.Button;
-
 import com.wj.myapplication.base.BaseActivity;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 
 public class MainActivity extends BaseActivity {
