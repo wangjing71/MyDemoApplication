68048b34f6b0d67beb5409b5ecd7166f6cd3f85b
397383523@qq.com <8362781wj>
2019-08-16 15:30:25 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/view/MyTextView.java
+++ app/src/main/java/com/wj/myapplication/view/MyTextView.java
@@ -1,6 +1,8 @@
 package com.wj.myapplication.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatTextView;
@@ -11,15 +13,27 @@ import androidx.appcompat.widget.AppCompatTextView;
  * Description
  */
 public class MyTextView extends AppCompatTextView {
+    private Paint paint1,paint2;
     public MyTextView(Context context) {
         super(context);
+        init();
+    }
+
+    private void init() {
     }
 
     public MyTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
