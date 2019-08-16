87ac3d6900e9a841c025fff6bbf31a3c27cff74c
397383523@qq.com <8362781wj>
2019-08-16 14:20:25 +0800

    千秋李

---
+++ app/src/main/java/com/wj/myapplication/activity/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.wj.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wj.myapplication.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
