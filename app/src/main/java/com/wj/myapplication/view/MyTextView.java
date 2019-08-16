7447918b11300e2e451d4c246d4a94aae52572b2
397383523@qq.com <8362781wj>
2019-08-16 15:02:17 +0800

    千秋李

---
+++ app/src/main/java/com/wj/myapplication/view/MyTextView.java
@@ -0,0 +1,11 @@
+package com.wj.myapplication.view;
+
+import android.widget.TextView;
+
+/**
+ * author wangjing
+ * Date 2019/8/16
+ * Description
+ */
+public class MyTextView extends android.support.v7.widget.AppCompatTextView {
+}
