279a0f1b8949e832e26259c06d87c133bcd95352
397383523@qq.com <8362781wj>
2019-08-16 14:29:11 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.wj.myapplication.activity.Main2Activity;
 import com.wj.myapplication.base.BaseActivity;
 
 import butterknife.BindView;
@@ -32,6 +34,8 @@ public class MainActivity extends BaseActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.button1:
+                Intent intent = new Intent(this, Main2Activity.class);
+                startActivity(intent);
                 break;
             case R.id.button2:
                 break;
---
+++ app/src/main/java/com/wj/myapplication/view/MyView.java
@@ -0,0 +1,49 @@
+package com.wj.myapplication.view;
+
+import android.content.Context;
+import android.icu.util.Measure;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * author wangjing
+ * Date 2019/8/16
+ * Description
+ */
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measureSpec(widthMeasureSpec),measureSpec(heightMeasureSpec));
+    }
+
+    private int measureSpec(int heightMeasureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(heightMeasureSpec);
+        int specSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if(specMode == MeasureSpec.EXACTLY){
+            result = specSize;
+        }else{
+            result = 200;
+            if(specMode == MeasureSpec.AT_MOST){
+                result = Math.min(result,specSize);
+            }
+        }
+
+        return  result;
+    }
+}
