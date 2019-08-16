98e62c9508f31f37369fe74b05f18f57953677c0
397383523@qq.com <8362781wj>
2019-08-16 15:55:53 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/view/MyTextView.java
+++ app/src/main/java/com/wj/myapplication/view/MyTextView.java
@@ -3,7 +3,10 @@ package com.wj.myapplication.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatTextView;
@@ -14,7 +17,12 @@ import androidx.appcompat.widget.AppCompatTextView;
  * Description
  */
 public class MyTextView extends AppCompatTextView {
-    private Paint paint1,paint2;
+    private Paint paint1, paint2;
+    private int mViewWidth;
+    private Matrix matrix;
+    private int mTransLate;
+    private LinearGradient linearGradient;
+
     public MyTextView(Context context) {
         super(context);
         init();
@@ -41,12 +49,44 @@ public class MyTextView extends AppCompatTextView {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                Paint paint = getPaint();
+                linearGradient = new LinearGradient(
+                        0,
+                        0,
+                        mViewWidth,
+                        0,
+                        new int[]{Color.BLUE, 0xffffffff, Color.BLUE},
+                        null,
+                        Shader.TileMode.CLAMP
+                );
+                paint.setShader(linearGradient);
+                matrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawRect(0,0,getMeasuredWidth(),getMeasuredHeight(),paint1);
-        canvas.drawRect(2,2,getMeasuredWidth()-2,getMeasuredHeight()-2,paint2);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint1);
+        canvas.drawRect(2, 2, getMeasuredWidth() - 2, getMeasuredHeight() - 2, paint2);
         canvas.save();
-
-
+        canvas.translate(0, 10);
         super.onDraw(canvas);
+        canvas.restore();
+
+        if(matrix != null){
+            mTransLate += mViewWidth/5;
+            if(mTransLate>2*mViewWidth){
+                mTransLate = -mViewWidth;
+            }
+            matrix.setTranslate(mTransLate,0);
+            linearGradient.setLocalMatrix(matrix);
+            postInvalidateDelayed(100);
+        }
     }
 }
