555200ce3cf16df6c4bfe82ef4ebb31ae90f3f0d
397383523@qq.com <8362781wj>
2019-08-21 16:31:07 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/ZxingActivity.java
+++ app/src/main/java/com/wj/myapplication/ZxingActivity.java
@@ -5,10 +5,6 @@ import android.content.Intent;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import cn.bingoogolapple.qrcode.core.QRCodeView;
-import cn.bingoogolapple.qrcode.zbar.ZBarView;
 
 public class ZxingActivity extends AppCompatActivity {
 
