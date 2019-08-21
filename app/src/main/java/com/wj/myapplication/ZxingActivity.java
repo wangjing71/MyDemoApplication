f1cc340f7f0c6d9cbd886f75e59da6a68e67d20c
397383523@qq.com <8362781wj>
2019-08-21 09:46:46 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/ZxingActivity.java
+++ app/src/main/java/com/wj/myapplication/ZxingActivity.java
@@ -3,11 +3,34 @@ package com.wj.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zbar.ZBarView;
+
 public class ZxingActivity extends AppCompatActivity {
+    private ZBarView mZBarView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zxing);
+
+        mZBarView = findViewById(R.id.zbarview);
+        mZBarView.setDelegate(new QRCodeView.Delegate() {
+            @Override
+            public void onScanQRCodeSuccess(String result) {
+
+            }
+
+            @Override
+            public void onCameraAmbientBrightnessChanged(boolean isDark) {
+
+            }
+
+            @Override
+            public void onScanQRCodeOpenCameraError() {
+
+            }
+        });
     }
 }
