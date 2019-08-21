4b3a3b2a5f5e104d9af35f2c36276c5e12be41a0
397383523@qq.com <8362781wj>
2019-08-21 09:51:08 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/ZxingActivity.java
+++ app/src/main/java/com/wj/myapplication/ZxingActivity.java
@@ -1,12 +1,14 @@
 package com.wj.myapplication;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import cn.bingoogolapple.qrcode.core.QRCodeView;
 import cn.bingoogolapple.qrcode.zbar.ZBarView;
 
-public class ZxingActivity extends AppCompatActivity {
+public class ZxingActivity extends AppCompatActivity implements QRCodeView.Delegate{
     private ZBarView mZBarView;
 
 
@@ -16,21 +18,60 @@ public class ZxingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_zxing);
 
         mZBarView = findViewById(R.id.zbarview);
-        mZBarView.setDelegate(new QRCodeView.Delegate() {
-            @Override
-            public void onScanQRCodeSuccess(String result) {
+        mZBarView.setDelegate(this);
+    }
 
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        Log.i("====", "result:" + result);
+        vibrate();
+//        mZBarView.startSpot(); // 开始识别
+    }
+
+    @Override
+    public void onCameraAmbientBrightnessChanged(boolean isDark) {
+        String tipText = mZBarView.getScanBoxView().getTipText();
+        String ambientBrightnessTip = "\n环境过暗，请打开闪光灯";
+        if (isDark) {
+            if (!tipText.contains(ambientBrightnessTip)) {
+                mZBarView.getScanBoxView().setTipText(tipText + ambientBrightnessTip);
             }
-
-            @Override
-            public void onCameraAmbientBrightnessChanged(boolean isDark) {
-
+        } else {
+            if (tipText.contains(ambientBrightnessTip)) {
+                tipText = tipText.substring(0, tipText.indexOf(ambientBrightnessTip));
+                mZBarView.getScanBoxView().setTipText(tipText);
             }
+        }
+    }
 
-            @Override
-            public void onScanQRCodeOpenCameraError() {
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        Log.e("====", "打开相机出错");
+    }
 
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mZBarView.startCamera(); // 打开后置摄像头开始预览，但是并未开始识别
+//        mZBarView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT); // 打开前置摄像头开始预览，但是并未开始识别
+
+        mZBarView.startSpotAndShowRect(); // 显示扫描框，并开始识别
+    }
+
+    @Override
+    protected void onStop() {
+        mZBarView.stopCamera(); // 关闭摄像头预览，并且隐藏扫描框
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mZBarView.onDestroy(); // 销毁二维码扫描控件
+        super.onDestroy();
+    }
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
     }
 }
