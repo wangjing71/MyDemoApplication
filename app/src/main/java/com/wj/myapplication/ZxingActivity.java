5d7534bdc98fdf1e7af44324f8799dd766fa267e
397383523@qq.com <8362781wj>
2019-08-21 16:33:06 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/ZxingActivity.java
+++ app/src/main/java/com/wj/myapplication/ZxingActivity.java
@@ -6,8 +6,14 @@ import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.bingoogolapple.qrcode.zxing.ZXingView;
+
 public class ZxingActivity extends AppCompatActivity {
 
+    private static final String TAG = ZxingActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY = 666;
+
+    private ZXingView mZXingView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
