a4335cec23d9cd26550d28c6c2cde8bfe9c46ef8
WangJing <397383523@qq.com>
2019-09-18 10:45:22 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import q.rorbin.badgeview.Badge;
 import q.rorbin.badgeview.QBadgeView;
@@ -12,6 +13,7 @@ import q.rorbin.badgeview.QBadgeView;
 public class MainActivity extends BaseActivity {
 
     private Button button;
+    private TextView textView;
 
     private Badge bar;
 
@@ -29,6 +31,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
     }
 
     @Override
@@ -49,5 +52,6 @@ public class MainActivity extends BaseActivity {
 
     private void doSomeThing() {
         bar.hide(true);
+        new QBadgeView(this).bindTarget(textView).setBadgeNumber(5).setBadgeBackgroundColor(Color.RED).setBadgeTextSize(8.0f,true);
     }
 }
