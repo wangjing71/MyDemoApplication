18872a60f3d2a516d3f47a626530bb4b5112de1c
WangJing <397383523@qq.com>
2019-10-23 13:41:22 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -33,7 +35,7 @@ public class MyView extends View {
 
     // 2.初始化画笔
     private void initPaint() {
-        mPaint.setColor(Color.BLACK);       //设置画笔颜色
+        mPaint.setColor(Color.RED);       //设置画笔颜色
         mPaint.setStyle(Paint.Style.FILL);  //设置画笔模式为填充
         mPaint.setStrokeWidth(10f);         //设置画笔宽度为10px
     }
@@ -51,7 +53,90 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.BLUE);
+//        canvas.drawColor(Color.BLUE);
+
+
+        //点
+//        canvas.drawPoint(200, 200, mPaint);     //在坐标(200,200)位置绘制一个点
+//        canvas.drawPoints(new float[]{          //绘制一组点，坐标位置由float数组指定
+//                500,500,
+//                500,600,
+//                500,700
+//        },mPaint);
+
+        //线
+//        canvas.drawLine(300,300,500,600,mPaint);    // 在坐标(300,300)(500,600)之间绘制一条直线
+//        canvas.drawLines(new float[]{               // 绘制一组线 每四数字(两个点的坐标)确定一条线
+//                100,200,200,200,
+//                100,300,200,300
+//        },mPaint);
+
+        //矩形
+        // 第一种
+//        canvas.drawRect(100,100,800,400,mPaint);
+
+        // 第二种
+//        Rect rect = new Rect(100,100,800,400);
+//        canvas.drawRect(rect,mPaint);
+
+// 第三种
+//        RectF rectF = new RectF(100,100,800,400);
+//        canvas.drawRect(rectF,mPaint);
+
+        //圆角矩形
+        // 第一种
+//        RectF rectF = new RectF(100,100,800,400);
+//        canvas.drawRoundRect(rectF,350,150,mPaint);
+
+        // 第二种
+//        canvas.drawRoundRect(100,100,800,400,30,30,mPaint);
+
+        //椭圆
+// 第一种
+//        RectF rectF = new RectF(100,100,800,400);
+//        canvas.drawOval(rectF,mPaint);
+
+        // 第二种
+//        canvas.drawOval(100,100,800,400,mPaint);
+
+        //圆
+//        canvas.drawCircle(500,500,400,mPaint);  // 绘制一个圆心坐标在(500,500)，半径为400 的圆
+
+//        RectF rectF = new RectF(100,100,800,400);
+//// 绘制背景矩形
+//        mPaint.setColor(Color.GRAY);
+//        canvas.drawRect(rectF,mPaint);
+
+// 绘制圆弧
+//        mPaint.setColor(Color.BLUE);
+//        canvas.drawArc(rectF,0,270,true,mPaint);
+
+
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(200,200,100,paint);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
