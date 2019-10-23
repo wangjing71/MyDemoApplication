d6cec62c70f67dffcab1135c3a0096f78c04a5cf
WangJing <397383523@qq.com>
2019-10-23 11:19:05 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -1,6 +1,9 @@
 package com.wj.myapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,15 +14,44 @@ import android.view.View;
  * Description
  */
 public class MyView extends View {
+    private Paint mPaint = new Paint();
+
     public MyView(Context context) {
         super(context);
+        initPaint();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initPaint();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    // 2.初始化画笔
+    private void initPaint() {
+        mPaint.setColor(Color.BLACK);       //设置画笔颜色
+        mPaint.setStyle(Paint.Style.FILL);  //设置画笔模式为填充
+        mPaint.setStrokeWidth(10f);         //设置画笔宽度为10px
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.BLUE);
+
     }
 }
