6f36a26b4cf54bad51d1f9cd506aedfecae878a3
WangJing <397383523@qq.com>
2019-10-23 15:34:05 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Picture;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
@@ -17,6 +18,7 @@ import android.view.View;
  */
 public class MyView extends View {
     private Paint mPaint = new Paint();
+    private Picture mPicture = new Picture();
 
     public MyView(Context context) {
         super(context);
@@ -40,6 +42,24 @@ public class MyView extends View {
         mPaint.setStrokeWidth(10f);         //设置画笔宽度为10px
     }
 
+    // 2.录制内容方法
+    private void recording() {
+        // 开始录制 (接收返回值Canvas)
+        Canvas canvas = mPicture.beginRecording(500, 500);
+        // 创建一个画笔
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL);
+
+        // 在Canvas中具体操作
+        // 位移
+        canvas.translate(250,250);
+        // 绘制一个圆
+        canvas.drawCircle(0,0,100,paint);
+
+        mPicture.endRecording();
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -53,103 +73,6 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        canvas.drawColor(Color.BLUE);
 
-
-        //点
-//        canvas.drawPoint(200, 200, mPaint);     //在坐标(200,200)位置绘制一个点
-//        canvas.drawPoints(new float[]{          //绘制一组点，坐标位置由float数组指定
-//                500,500,
-//                500,600,
-//                500,700
-//        },mPaint);
-
-        //线
-//        canvas.drawLine(300,300,500,600,mPaint);    // 在坐标(300,300)(500,600)之间绘制一条直线
-//        canvas.drawLines(new float[]{               // 绘制一组线 每四数字(两个点的坐标)确定一条线
-//                100,200,200,200,
-//                100,300,200,300
-//        },mPaint);
-
-        //矩形
-        // 第一种
-//        canvas.drawRect(100,100,800,400,mPaint);
-
-        // 第二种
-//        Rect rect = new Rect(100,100,800,400);
-//        canvas.drawRect(rect,mPaint);
-
-// 第三种
-//        RectF rectF = new RectF(100,100,800,400);
-//        canvas.drawRect(rectF,mPaint);
-
-        //圆角矩形
-        // 第一种
-//        RectF rectF = new RectF(100,100,800,400);
-//        canvas.drawRoundRect(rectF,350,150,mPaint);
-
-        // 第二种
-//        canvas.drawRoundRect(100,100,800,400,30,30,mPaint);
-
-        //椭圆
-// 第一种
-//        RectF rectF = new RectF(100,100,800,400);
-//        canvas.drawOval(rectF,mPaint);
-
-        // 第二种
-//        canvas.drawOval(100,100,800,400,mPaint);
-
-        //圆
-//        canvas.drawCircle(500,500,400,mPaint);  // 绘制一个圆心坐标在(500,500)，半径为400 的圆
-
-//        RectF rectF = new RectF(100,100,800,400);
-//// 绘制背景矩形
-//        mPaint.setColor(Color.GRAY);
-//        canvas.drawRect(rectF,mPaint);
-
-// 绘制圆弧
-//        mPaint.setColor(Color.BLUE);
-//        canvas.drawArc(rectF,0,270,true,mPaint);
-
-
-//
-//        Paint paint = new Paint();
-//        paint.setColor(Color.BLUE);
-//
-//        paint.setStyle(Paint.Style.STROKE);
-//        paint.setStrokeWidth(20);
-//        canvas.drawCircle(200,200,100,paint);
-
-//        mPaint.setColor(Color.BLACK);
-//        canvas.translate(getWidth() / 2, getHeight() / 2);
-//        canvas.drawCircle(0,0,100,mPaint);
-//
-//// 在坐标原点绘制一个蓝色圆形
-//        mPaint.setColor(Color.BLUE);
-//        canvas.translate(500,500);
-//        canvas.drawCircle(0,0,100,mPaint);
-
-
-//        canvas.translate(getWidth() / 2, getHeight() / 2);
-//
-//        RectF rect = new RectF(0,-400,400,0);   // 矩形区域
-//
-//        mPaint.setColor(Color.BLACK);           // 绘制黑色矩形
-//        canvas.drawRect(rect,mPaint);
-//
-//
-//        canvas.scale(0.5f,0.5f);                // 画布缩放
-//        mPaint.setColor(Color.BLUE);            // 绘制蓝色矩形
-//        canvas.drawRect(rect,mPaint);
-
-
-        canvas.translate(getWidth() / 2, getHeight() / 2);
-        mPaint.setStyle(Paint.Style.STROKE);
-        RectF rect = new RectF(-400, -400, 400, 400);   // 矩形区域
-
-        for (int i = 0; i <= 50; i++) {
-            canvas.scale(0.9f, 0.9f);
-            canvas.drawRect(rect, mPaint);
-        }
     }
 }
