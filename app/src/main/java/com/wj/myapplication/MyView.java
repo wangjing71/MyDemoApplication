45db7e1f53f4fd0d986a26ec0f2da5960a493387
WangJing <397383523@qq.com>
2019-10-24 13:39:29 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Picture;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -37,7 +38,7 @@ public class MyView extends View {
     // 2.初始化画笔
     private void initPaint() {
         mPaint.setColor(Color.RED);       //设置画笔颜色
-        mPaint.setStyle(Paint.Style.FILL);  //设置画笔模式为填充
+        mPaint.setStyle(Paint.Style.STROKE);  //设置画笔模式为填充
         mPaint.setStrokeWidth(10f);         //设置画笔宽度为10px
     }
 
@@ -54,5 +55,19 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.translate(getWidth() / 2, getHeight() / 2);  // 移动坐标系到屏幕中心(宽高数据在onSizeChanged中获取)
+        canvas.scale(1, -1);
+
+        Path path = new Path();
+        path.lineTo(100, 100);
+
+        RectF oval = new RectF(0, 0, 300, 300);
+
+        path.arcTo(oval, 0, 270, false);   // <-- 和上面一句作用等价
+
+        path.close();
+        canvas.drawPath(path, mPaint);
+
+
     }
 }
