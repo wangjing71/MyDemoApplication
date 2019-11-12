ae80fe6617477ea9348b8e8ae52d5c026518803d
WangJing <397383523@qq.com>
2019-11-12 15:48:04 +0800

    千秋李

---
+++ app/src/main/java/com/wj/myapplication/MyAdapter.java
@@ -0,0 +1,55 @@
+package com.wj.myapplication;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * author wangjing
+ * Date 2018/12/24
+ * Description
+ */
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+
+
+    public MyAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = null;
+        viewHolder = new MyItem(LayoutInflater.from(context).inflate(R.layout.item_business, parent, false));
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        MyItem myViewHolder = (MyItem) holder;
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    class MyItem extends RecyclerView.ViewHolder {
+
+        private TextView icon;
+
+
+        public MyItem(View view) {
+            super(view);
+            icon = view.findViewById(R.id.icon);
+        }
+    }
+
+}
