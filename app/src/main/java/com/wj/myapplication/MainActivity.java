0da70e035f5c7f2e9a47505b8a3db7ec79bad1d3
WangJing <397383523@qq.com>
2019-11-25 15:52:05 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,32 +1,36 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-
-public class MainActivity extends BaseActivity {
-
-
+/**
+ * Created by gavin
+ * date 2018/6/12
+ */
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected int setLayoutId() {
-        return R.layout.activity_main;
-    }
-
-    @Override
-    protected void initView() {
-    }
-
-    @Override
-    protected void initData() {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
     }
 
-    @Override
-    protected void setEvent() {
+    public void toScrollView(View view) {
+        Intent intent = new Intent(this, ScrollViewActivity.class);
+        startActivity(intent);
     }
 
-    private void doSomeThing() {
-
+    public void toRecyclerView(View view) {
+        Intent intent = new Intent(this, RecyclerViewActivity.class);
+        startActivity(intent);
     }
+
+    public void toCoordinatorLayout(View view) {
+        Intent intent = new Intent(this, CoordinatorLayoutActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/wj/myapplication/ScrollViewActivity.java
@@ -0,0 +1,68 @@
+package com.wj.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ScrollView;
+
+import com.gavin.view.flexible.FlexibleLayout;
+import com.gavin.view.flexible.callback.OnReadyPullListener;
+import com.gavin.view.flexible.callback.OnRefreshListener;
+
+/**
+ * Created by gavin
+ * date 2018/6/18
+ */
+public class ScrollViewActivity extends AppCompatActivity {
+
+    private View mHeader;
+    private ScrollView mScrollView;
+    private FlexibleLayout mFlexibleLayout;
+    private View mRefreshView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view);
+        initView();
+        mFlexibleLayout.setHeader(mHeader)
+                .setReadyListener(new OnReadyPullListener() {
+                    @Override
+                    public boolean isReady() {
+                        return mScrollView.getScrollY() == 0;
+                    }
+                })
+                .setRefreshable(true)
+                .setDefaultRefreshView(new OnRefreshListener() {
+                    @Override
+                    public void onRefreshing() {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Thread.sleep(2000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                //刷新完成后需要调用onRefreshComplete()通知FlexibleLayout
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mFlexibleLayout.onRefreshComplete();
+                                    }
+                                });
+                            }
+                        }).start();
+                    }
+                });
+    }
+
+    private void initView() {
+        mFlexibleLayout = (FlexibleLayout) findViewById(R.id.ffv);
+        mHeader = findViewById(R.id.header);
+        mScrollView = (ScrollView) findViewById(R.id.sv);
+        mRefreshView = LayoutInflater.from(this).inflate(R.layout.refresh_layout, null);
+    }
+}
