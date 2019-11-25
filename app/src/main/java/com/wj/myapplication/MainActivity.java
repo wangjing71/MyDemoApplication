86fa794e3025988b66fabea9dcb7672c934e261d
WangJing <397383523@qq.com>
2019-11-25 15:45:53 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -7,13 +7,6 @@ import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
-    private Button button;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     protected int setLayoutId() {
@@ -22,7 +15,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        button = findViewById(R.id.button);
     }
 
     @Override
@@ -32,12 +24,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doSomeThing();
-            }
-        });
     }
 
     private void doSomeThing() {
