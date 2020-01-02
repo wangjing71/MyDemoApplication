b15acb3aad501126b58cac03c14f900c9e93577a
WangJing <397383523@qq.com>
2020-01-02 14:49:31 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        button = findViewById(R.id.button);
     }
 
     @Override
@@ -32,12 +31,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doSomeThing();
-            }
-        });
     }
 
     private void doSomeThing() {
