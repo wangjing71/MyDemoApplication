fa7d9d1e860fe8205a5cfd012ccb9181a6aaeef6
WangJing <397383523@qq.com>
2020-01-02 14:20:27 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/BaseActivity.java
+++ app/src/main/java/com/wj/myapplication/BaseActivity.java
@@ -1,19 +1,14 @@
 package com.wj.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
 
 /**
  * Created by Administrator on 2018/1/4.
  */
 
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
