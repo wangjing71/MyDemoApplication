90a211d1ace4c97ec58c51168b5e481df8bcd888
WangJing <397383523@qq.com>
2020-01-02 14:18:54 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/BaseActivity.java
+++ app/src/main/java/com/wj/myapplication/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.wj.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,7 @@ import com.tbruyelle.rxpermissions2.RxPermissions;
  * Created by Administrator on 2018/1/4.
  */
 
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends Activity {
 
 
     protected RxPermissions rxPermissions;
@@ -22,7 +23,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutId());
-        rxPermissions = new RxPermissions(this);
         barView = findViewById(R.id.barView);
         initView();
         initData();
