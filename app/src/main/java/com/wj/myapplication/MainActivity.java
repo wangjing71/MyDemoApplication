eb78979ec9eb0eb046156ac94d0dd5148fd262b0
WangJing <397383523@qq.com>
2020-01-08 09:38:32 +0800

    千秋李

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.gyf.immersionbar.ImmersionBar;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.SimpleMultiPurposeListener;
 
 
 public class MainActivity extends BaseActivity {
+    private int mOffset = 0;
+    private int mScrollY = 0;
 
-    private Button button;
+    private int imageHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +33,52 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        button = findViewById(R.id.button);
+        ImmersionBar.with(this).init();
     }
 
     @Override
     protected void initData() {
 
-    }
+        //状态栏透明和间距处理
+//        StatusBarUtil.immersive(this);
+//        StatusBarUtil.setPaddingSmart(this, toolbar);
+//        StatusBarUtil.setMargin(this, findViewById(R.id.header));
 
-    @Override
-    protected void setEvent() {
-        button.setOnClickListener(new View.OnClickListener() {
+        final View parallax = findViewById(R.id.parallax);
+        parallax.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
-            public void onClick(View v) {
-                doSomeThing();
+            public void onGlobalLayout() {
+                imageHeight = parallax.getHeight();
+            }
+        });
+        final RefreshLayout refreshLayout = findViewById(R.id.refreshLayout);
+
+
+        refreshLayout.setOnMultiPurposeListener(new SimpleMultiPurposeListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                refreshLayout.finishRefresh(3000);
+            }
+
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                refreshLayout.finishLoadMore(2000);
+            }
+
+            @Override
+            public void onHeaderMoving(RefreshHeader header, boolean isDragging, float percent, int offset, int headerHeight, int maxDragHeight) {
+                Log.i("====offset", offset + "");
+                mOffset = offset / 2;
+//                parallax.setTranslationY(mOffset - mScrollY);
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) parallax.getLayoutParams();
+                params.height = imageHeight + offset;
+                parallax.setLayoutParams(params);
             }
         });
     }
 
-    private void doSomeThing() {
-
+    @Override
+    protected void setEvent() {
     }
+
 }
