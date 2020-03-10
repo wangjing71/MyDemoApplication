98f45ae9c94b0ec0cf993b9b5239e2530407630b
WangJing <397383523@qq.com>
2020-03-10 10:26:47 +0800

    xml颜色渐变

---
+++ app/src/main/java/com/wj/myapplication/ChildFragment.java
@@ -0,0 +1,24 @@
+package com.wj.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * author wangjing
+ * Date 2020/3/10
+ * Description
+ */
+public class ChildFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.child_fragment,null,false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
