cc7acccc68ee21f946184f068b1ee491caaa8aec
WangJing <397383523@qq.com>
2020-03-17 14:41:47 +0800

    xml颜色渐变

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,18 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends BaseActivity {
 
     private Button button;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,17 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         button = findViewById(R.id.button);
+        tv = findViewById(R.id.tv);
     }
 
     @Override
     protected void initData() {
-
+        String text = "2020年3月31日";
+        Spannable textSpan = new SpannableStringBuilder(text);
+        textSpan.setSpan(new AbsoluteSizeSpan(30), 0, text.indexOf("年"), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        textSpan.setSpan(new AbsoluteSizeSpan(50), text.indexOf("年"), text.indexOf("月"), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        textSpan.setSpan(new AbsoluteSizeSpan(30), text.indexOf("月"), text.indexOf("日"), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        tv.setText(textSpan);
     }
 
     @Override
