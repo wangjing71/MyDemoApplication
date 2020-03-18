e7787e115e58564398422e0bdefbd719872d0cbb
WangJing <397383523@qq.com>
2020-03-18 17:50:33 +0800

    xml颜色渐变

---
+++ app/src/main/java/com/wj/myapplication/Demo4Activity.java
@@ -0,0 +1,26 @@
+package com.wj.myapplication;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class Demo4Activity extends BaseActivity {
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.aaa;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+    @Override
+    protected void setEvent() {
+
+    }
+}
