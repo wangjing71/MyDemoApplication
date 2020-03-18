2ec02936b473eea9ea023a7c78607d2a82267f49
WangJing <397383523@qq.com>
2020-03-18 16:28:47 +0800

    xml颜色渐变

--- app/src/main/java/com/wj/myapplication/Demo3Activity.java
+++ app/src/main/java/com/wj/myapplication/Demo3Activity.java
@@ -1,8 +1,12 @@
 package com.wj.myapplication;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class Demo3Activity extends BaseActivity {
     private RecyclerView recyclerView;
@@ -24,6 +28,18 @@ public class Demo3Activity extends BaseActivity {
     protected void initData() {
 //        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 //        recyclerView.setAdapter(new MyTextAdapter(this));
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"data delete",Snackbar.LENGTH_LONG).setAction("确定", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(Demo3Activity.this, "222222222", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        });
     }
 
     @Override
