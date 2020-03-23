bec3aa7bdd7c2b68af921b653309e949f384e60b
WangJing <397383523@qq.com>
2020-03-23 17:25:58 +0800

    xml颜色渐变

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -2,11 +2,13 @@ package com.wj.myapplication;
 
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 
 public class MainActivity extends BaseActivity {
     private LinearLayout parent;
+    private Button button;
 
     @Override
     protected int setLayoutId() {
@@ -16,26 +18,32 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         parent = findViewById(R.id.parent);
+        button = findViewById(R.id.button);
     }
 
     @Override
     protected void initData() {
-        for (int i = 0; i < 4; i++) {
-            MyExpandView view1 = (MyExpandView) LayoutInflater.from(this).inflate(R.layout.items_layout, null, false);
-            view1.initExpand(false);
-            view1.expandDelayed(1500 + 500 * i);
-            View view2 = LayoutInflater.from(this).inflate(R.layout.chindviewlauouit, null, false);
-            view1.addExpandView(view2);
-            parent.addView(view1);
-        }
+
     }
 
     @Override
     protected void setEvent() {
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeThing();
+            }
+        });
     }
 
     private void doSomeThing() {
-
+        for (int i = 0; i < 4; i++) {
+            MyExpandView view1 = (MyExpandView) LayoutInflater.from(this).inflate(R.layout.items_layout, null, false);
+            view1.initExpand(false);
+            View view2 = LayoutInflater.from(this).inflate(R.layout.chindviewlauouit, null, false);
+            view1.addExpandView(view2);
+            parent.addView(view1);
+            view1.expandDelayed(200 + 500 * i);
+        }
     }
 }
