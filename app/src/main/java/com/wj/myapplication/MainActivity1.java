379ff99a2b3be5b994f2d3ec3625168dc5181b8d
WangJing <397383523@qq.com>
2020-03-24 16:42:25 +0800

    xml颜色渐变

--- app/src/main/java/com/wj/myapplication/MainActivity1.java
+++ app/src/main/java/com/wj/myapplication/MainActivity1.java
@@ -1,7 +1,9 @@
 package com.wj.myapplication;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -9,6 +11,7 @@ import android.widget.LinearLayout;
 
 public class MainActivity1 extends BaseActivity {
     private LinearLayout parent;
+    private long animateTime = 200;
 
     @Override
     protected int setLayoutId() {
@@ -27,19 +30,49 @@ public class MainActivity1 extends BaseActivity {
         moveToTop(0, view1);
         View view2 = LayoutInflater.from(MainActivity1.this).inflate(R.layout.item1, null, false);
         parent.addView(view2);
-        moveToTop(500, view2);
+        moveToTop(animateTime, view2);
+        View view3 = LayoutInflater.from(MainActivity1.this).inflate(R.layout.item1, null, false);
+        parent.addView(view3);
+        moveToTop(animateTime * 2, view3);
+
+        View view4 = LayoutInflater.from(MainActivity1.this).inflate(R.layout.item1, null, false);
+        parent.addView(view4);
+        moveToTop(animateTime * 3, view4);
     }
 
     @Override
     protected void setEvent() {
+
     }
 
-    public void moveToTop(long startDelay, View view) {
+    public void moveToTop(long startDelay, final View view) {
+        view.setVisibility(View.GONE);
         ObjectAnimator translationY = new ObjectAnimator().ofFloat(view, "translationY", 600, 0);
-        AnimatorSet animatorSet = new AnimatorSet();  //组合动画
-        animatorSet.playTogether(translationY); //设置动画
-        animatorSet.setDuration(500);  //设置动画时间
-        animatorSet.setStartDelay(startDelay);
-        animatorSet.start(); //启动
+        translationY.setStartDelay(10 + startDelay);
+        translationY.setDuration(animateTime);
+        translationY.start(); //启动
+
+        translationY.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                Log.i("====", "onAnimationStart");
+                view.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Log.i("====", "onAnimationEnd");
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                Log.i("====", "onAnimationCancel");
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
