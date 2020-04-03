c9de593ff76082c8de03ecf65177a608d3acb281
WangJing <397383523@qq.com>
2020-04-03 16:27:23 +0800

    xml颜色渐变

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -8,12 +8,6 @@ import android.widget.Button;
 public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
     protected int setLayoutId() {
         return R.layout.activity_main;
     }
