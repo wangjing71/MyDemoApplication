92576fe6e4e97c17de9bc9cedced622fa3202a31
WangJing <397383523@qq.com>
2020-04-03 16:27:53 +0800

    xml颜色渐变

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,10 +1,5 @@
 package com.wj.myapplication;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-
 public class MainActivity extends BaseActivity {
 
     @Override
