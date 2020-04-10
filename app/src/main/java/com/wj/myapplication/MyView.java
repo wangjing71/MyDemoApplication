0a60ef4f348f273ec5f1fedca6dd151bc2a06261
WangJing <397383523@qq.com>
2020-04-10 10:39:47 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -46,5 +46,7 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        canvas.drawCircle(length/2,length/2,length/4,paint);
     }
 }
