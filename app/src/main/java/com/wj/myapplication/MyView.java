68a9619e1157eec5b3388fc50a57ffeea67a79d6
WangJing <397383523@qq.com>
2020-04-10 10:10:56 +0800

    tab 完美

---
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -0,0 +1,25 @@
+package com.wj.myapplication;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * author wangjing
+ * Date 2020/4/10
+ * Description
+ */
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
