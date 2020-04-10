f67c589c4386628f90db14e63c532aafd0ebbc24
WangJing <397383523@qq.com>
2020-04-10 10:29:54 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -1,6 +1,10 @@
 package com.wj.myapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,15 +15,36 @@ import android.view.View;
  * Description
  */
 public class MyView extends View {
+    private Paint paint;
+
+    private RectF mRectf;
+
+    private int length;
+
     public MyView(Context context) {
         super(context);
+        init();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        length = getWidth();
+        mRectf = new RectF((float) (length * 0.1),(float) (length * 0.1),(float) (length * 0.9),(float) (length * 0.9));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
