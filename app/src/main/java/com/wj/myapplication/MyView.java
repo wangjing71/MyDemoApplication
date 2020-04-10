ec505d27ce0574a83e296d2c2c3e793f8e22f003
WangJing <397383523@qq.com>
2020-04-10 10:46:20 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MyView.java
+++ app/src/main/java/com/wj/myapplication/MyView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -39,14 +40,21 @@ public class MyView extends View {
     private void init() {
         paint = new Paint();
         paint.setColor(Color.RED);
+        paint.setAntiAlias(false); //设置画笔为无锯齿
+        paint.setStrokeWidth((float) 3.0); //线宽
+        paint.setStyle(Paint.Style.STROKE); //空心效果
+
         length = getWidth();
-        mRectf = new RectF((float) (length * 0.1),(float) (length * 0.1),(float) (length * 0.9),(float) (length * 0.9));
+        Log.i("====",length+"");
+        mRectf = new RectF((float) (length * 0.1), (float) (length * 0.1), (float) (length * 0.9), (float) (length * 0.9));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(length/2,length/2,length/4,paint);
+        canvas.drawCircle(length / 2, length / 2, length / 4, paint);
+
+        canvas.drawCircle(50, 50, 10, paint); //绘制圆形
     }
 }
