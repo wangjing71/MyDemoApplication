f8e937d6616035b93268cee477e28b4b38d9827d
WangJing <397383523@qq.com>
2020-04-20 15:07:53 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MyApplication.java
+++ app/src/main/java/com/wj/myapplication/MyApplication.java
@@ -43,6 +43,8 @@ public class MyApplication extends Application {
             @Override
             public void onActivityPaused(Activity activity) {
                 Log.d("====", activity + "onActivityPaused");
+                currentActivity = (BaseActivity) activity;
+                currentActivity.showMohu();
             }
 
             @Override
@@ -65,20 +67,5 @@ public class MyApplication extends Application {
     }
 
     private void initBackFrontGround() {
-        AppFrontBackHelper helper = new AppFrontBackHelper();
-        helper.register(MyApplication.this, new AppFrontBackHelper.OnAppStatusListener() {
-            @Override
-            public void onFront() {
-                //应用切到前台处理
-//                Toast.makeText(RichenInfoApplication.this, "前台", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onBack() {
-                //应用切到后台处理
-                currentActivity.showMohu();
-                Toast.makeText(MyApplication.this, "上海移动和你已切换到后台运行", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
