f35c32fd9a42f34c38d34100ad0121c0a6317bfa
WangJing <397383523@qq.com>
2020-04-20 15:05:44 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MyApplication.java
+++ app/src/main/java/com/wj/myapplication/MyApplication.java
@@ -1,6 +1,9 @@
 package com.wj.myapplication;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.tencent.bugly.Bugly;
@@ -11,11 +14,54 @@ import com.tencent.bugly.Bugly;
  * Description
  */
 public class MyApplication extends Application {
+
+    public BaseActivity currentActivity;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Bugly.init(getApplicationContext(), "9c59c60539", false);
         initBackFrontGround();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                Log.d("====", activity + "onActivityCreated");
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                Log.d("====", activity + "onActivityStarted");
+                currentActivity = (BaseActivity) activity;
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                Log.d("====", activity + "onActivityResumed");
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                Log.d("====", activity + "onActivityPaused");
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                Log.d("====", activity + "onActivityStopped");
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+                Log.d("====", activity + "onActivitySaveInstanceState");
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                Log.d("====", activity + "onActivityDestroyed");
+            }
+        });
+
+
     }
 
     private void initBackFrontGround() {
@@ -30,6 +76,7 @@ public class MyApplication extends Application {
             @Override
             public void onBack() {
                 //应用切到后台处理
+                currentActivity.showMohu();
                 Toast.makeText(MyApplication.this, "上海移动和你已切换到后台运行", Toast.LENGTH_SHORT).show();
             }
         });
