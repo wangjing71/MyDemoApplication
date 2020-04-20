1e74a98ce89945e6eb5abe41503b6f12b2dbf9de
WangJing <397383523@qq.com>
2020-04-20 15:22:35 +0800

    tab 完美

---
+++ app/src/main/java/com/wj/myapplication/A.java
@@ -0,0 +1,28 @@
+package com.wj.myapplication;
+
+/**
+ * author wangjing
+ * Date 2020/4/20
+ * Description
+ */
+public class A extends BaseActivity {
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_main_1;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setEvent() {
+
+    }
+}
