be5418d7798e1a84dd364e4aadc64c400d7c5ec3
WangJing <397383523@qq.com>
2020-04-24 15:43:43 +0800

    tab 完美

---
+++ app/src/main/java/com/wj/myapplication/MyRelativeLayout.java
@@ -0,0 +1,24 @@
+package com.wj.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * author wangjing
+ * Date 2020/4/24
+ * Description
+ */
+public class MyRelativeLayout extends RelativeLayout {
+    public MyRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public MyRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
