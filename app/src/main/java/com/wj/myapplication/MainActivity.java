611f79ebb30b1896d3b15adea618d5caaadd38c3
WangJing <397383523@qq.com>
2020-04-24 15:32:44 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -3,11 +3,12 @@ package com.wj.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends BaseActivity {
 
-//    private Button button;
+    private Button button;
 
     @Override
     protected int setLayoutId() {
@@ -16,7 +17,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-//        button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
     }
 
     @Override
@@ -26,15 +27,16 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                doSomeThing();
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeThing();
+            }
+        });
     }
 
     private void doSomeThing() {
+        Toast.makeText(this, "按钮被点击了", Toast.LENGTH_SHORT).show();
 
     }
 }
