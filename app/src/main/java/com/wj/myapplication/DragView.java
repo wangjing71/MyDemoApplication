09404f651ea8c84627ac1b2d00a4013bd0f63199
WangJing <397383523@qq.com>
2020-05-11 11:04:16 +0800

    tab 完美

---
+++ app/src/main/java/com/wj/myapplication/DragView.java
@@ -0,0 +1,27 @@
+package com.wj.myapplication;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * author wangjing
+ * Date 2020/5/11
+ * Description
+ */
+public class DragView extends View {
+    public DragView(Context context) {
+        super(context);
+    }
+
+    public DragView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DragView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
