626613fec3ab46b25cc83107694abf800dd5a982
WangJing <397383523@qq.com>
2020-05-11 13:27:30 +0800

    tab 完美

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -3,11 +3,13 @@ package com.wj.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends BaseActivity {
 
 //    private Button button;
+    private LinearLayout parent;
 
     @Override
     protected int setLayoutId() {
@@ -16,12 +18,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        parent = findViewById(R.id.parent);
     }
 
     @Override
     protected void initData() {
-
+        DragView.addDragView(this,parent,R.layout.drag_view);
     }
 
     @Override
