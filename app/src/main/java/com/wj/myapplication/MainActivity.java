3643587cd188f4a6711838992f87e51aee33adf8
WangJing <397383523@qq.com>
2020-05-15 13:45:07 +0800

    View跟随手指滑动

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -7,8 +7,6 @@ import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
-//    private Button button;
-
     @Override
     protected int setLayoutId() {
         return R.layout.activity_main;
@@ -16,7 +14,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-//        button = findViewById(R.id.button);
     }
 
     @Override
@@ -26,15 +23,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                doSomeThing();
-//            }
-//        });
-    }
-
-    private void doSomeThing() {
 
     }
 }
