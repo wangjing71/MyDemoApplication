7d4e9cae9a4c3e0212b837fcdf6595495356f50e
WangJing <397383523@qq.com>
2020-05-18 09:43:33 +0800

    View跟随手指滑动

---
+++ app/src/main/java/com/wj/myapplication/vp/MyTextRollView.java
@@ -0,0 +1,25 @@
+package com.wj.myapplication.vp;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * author wangjing
+ * Date 2020/5/18
+ * Description
+ */
+public class MyTextRollView extends LinearLayout {
+    public MyTextRollView(Context context) {
+        super(context);
+    }
+
+    public MyTextRollView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextRollView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
