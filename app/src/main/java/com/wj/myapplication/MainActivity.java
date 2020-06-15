6ad0f553fdbcbe478c41b57c34c577fe5ad15b87
WangJing <397383523@qq.com>
2020-06-15 15:33:31 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -2,23 +2,32 @@ package com.wj.myapplication;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.tbruyelle.rxpermissions2.Permission;
 
+import java.io.File;
+
 import io.reactivex.functions.Consumer;
+
+import static android.media.MediaMetadataRetriever.OPTION_CLOSEST_SYNC;
 
 
 public class MainActivity extends BaseActivity {
 
     private Button button;
+    private ImageView show;
 
     @Override
     protected int setLayoutId() {
@@ -28,6 +37,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         button = findViewById(R.id.button);
+        show = findViewById(R.id.show);
     }
 
     @Override
@@ -41,6 +51,18 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 doSomeThing();
+            }
+        });
+
+        findViewById(R.id.get).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+                String path= Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        File.separator + "video.mp4";
+                mmr.setDataSource(path);
+                Bitmap bitmap = mmr.getFrameAtTime(1*1000*1000,OPTION_CLOSEST_SYNC );//获取1秒附近的关键帧
+                show.setImageBitmap(bitmap);
             }
         });
     }
