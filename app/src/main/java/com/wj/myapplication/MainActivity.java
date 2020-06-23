a1c7e7fa117eaf25985f97e7469b85cd1d4b632d
WangJing <397383523@qq.com>
2020-06-23 18:06:56 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.wj.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
+
 
 public class MainActivity extends BaseActivity {
 
-//    private Button button;
+    private Button button;
 
     @Override
     protected int setLayoutId() {
@@ -16,7 +22,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-//        button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
     }
 
     @Override
@@ -26,15 +32,23 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                doSomeThing();
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeThing();
+            }
+        });
     }
 
     private void doSomeThing() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) {
+                        startActivity(new Intent(MainActivity.this,VideoRecordActivity.class));
+                    }
+                });
 
     }
 }
