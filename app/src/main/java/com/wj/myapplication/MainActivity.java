923409cec1297207b97e5d703b6d3f7324a43838
WangJing <397383523@qq.com>
2020-06-24 11:05:49 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -2,10 +2,12 @@ package com.wj.myapplication;
 
 import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import io.reactivex.functions.Consumer;
 
@@ -45,12 +47,50 @@ public class MainActivity extends BaseActivity {
                 .subscribe(new Consumer<Boolean>() {
                     @Override
                     public void accept(Boolean aBoolean) {
-                        if(aBoolean){
+                        if (aBoolean) {
                             Intent jumpIntent = new Intent(Intent.ACTION_PICK);
                             jumpIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
                             startActivityForResult(jumpIntent, SELECT_CONTACT);
                         }
                     }
                 });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case SELECT_CONTACT:
+                    //选择通讯录联系人返回
+                    if (data == null) {
+                        return;
+                    }
+                    try {
+                        if (data.getData() != null) {
+                            Cursor cursor = getContentResolver()
+                                    .query(data.getData(),
+                                            new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},
+                                            null, null, null);
+
+                            while (cursor.moveToNext()) {
+                                //取出该条数据的联系人姓名
+                                String name = cursor.getString(1).replaceAll(" ", "");
+                                //取出该条数据的联系人的手机号
+                                String number = cursor.getString(0).replaceAll(" ", "").replaceAll("-", "");
+                                Toast.makeText(this, name + "_" + number, Toast.LENGTH_SHORT).show();
+                            }
+                            cursor.close();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
