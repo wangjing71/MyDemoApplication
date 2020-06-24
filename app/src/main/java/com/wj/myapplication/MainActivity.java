7ee0ded582b1ddcc16f3681c71e5b500f58ccc2c
WangJing <397383523@qq.com>
2020-06-24 10:59:25 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package com.wj.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends BaseActivity {
 
+    private static final int SELECT_CONTACT = 1001;
     private Button button;
 
     @Override
@@ -35,6 +38,8 @@ public class MainActivity extends BaseActivity {
     }
 
     private void doSomeThing() {
-
+        Intent jumpIntent = new Intent(Intent.ACTION_PICK);
+        jumpIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(jumpIntent, SELECT_CONTACT);
     }
 }
