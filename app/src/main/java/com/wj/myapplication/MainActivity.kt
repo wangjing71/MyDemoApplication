cebf601b0c7d4aea09f4b793f0d6c67949ce7421
WangJing <397383523@qq.com>
2020-06-28 15:17:40 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.kt
+++ app/src/main/java/com/wj/myapplication/MainActivity.kt
@@ -17,19 +17,4 @@ class MainActivity : BaseActivity() {
 
     override fun setEvent() {
     }
-
-    fun getResult(a: Int, b: Int): Int {
-        return a + b
-    }
-
-    fun getResult1(a: Int, b: Int) = a + b
-
-    fun getResult3(a: Int, b: Int){
-        print(a + b)
-        if (a > b) {
-
-        }else{
-
-        }
-    }
 }
