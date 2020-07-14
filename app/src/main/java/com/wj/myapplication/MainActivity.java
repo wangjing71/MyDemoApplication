196f591fc33caafbee18ab4dd843945aeb1f94ec
WangJing <397383523@qq.com>
2020-07-14 17:21:00 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.wj.myapplication;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends BaseActivity {
 
 //    private Button button;
+    private ImageView show;
 
     @Override
     protected int setLayoutId() {
@@ -17,10 +21,21 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
 //        button = findViewById(R.id.button);
+        show = findViewById(R.id.show);
     }
 
     @Override
     protected void initData() {
+        AnimationDrawable animationDrawable1 = new AnimationDrawable();
+        int[] ids = {R.mipmap.a1,R.mipmap.a2,R.mipmap.a3,R.mipmap.a4};
+        for(int i = 0 ; i < 4 ; i ++){
+            Drawable frame = getResources().getDrawable(ids[i]);
+            //设定时长
+            animationDrawable1.addFrame(frame,200);
+        }
+        animationDrawable1.setOneShot(false);
+        show.setBackground(animationDrawable1);
+        animationDrawable1.start();
 
     }
 
