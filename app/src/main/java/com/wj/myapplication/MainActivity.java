22babe6ca7aa5c1b00a13b203e5692e6bfb95026
WangJing <397383523@qq.com>
2020-07-14 17:31:12 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends BaseActivity {
 
-//    private Button button;
+    private Button button;
     private ImageView show;
+    private AnimationDrawable animationDrawable1;
 
     @Override
     protected int setLayoutId() {
@@ -20,36 +21,35 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-//        button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
         show = findViewById(R.id.show);
     }
 
     @Override
     protected void initData() {
-        AnimationDrawable animationDrawable1 = new AnimationDrawable();
+        animationDrawable1 = new AnimationDrawable();
         int[] ids = {R.mipmap.a1,R.mipmap.a2,R.mipmap.a3,R.mipmap.a4};
         for(int i = 0 ; i < 4 ; i ++){
             Drawable frame = getResources().getDrawable(ids[i]);
             //设定时长
-            animationDrawable1.addFrame(frame,200);
+            animationDrawable1.addFrame(frame,150);
         }
         animationDrawable1.setOneShot(false);
         show.setBackground(animationDrawable1);
-        animationDrawable1.start();
-
+        animationDrawable1.setOneShot(true);
     }
 
     @Override
     protected void setEvent() {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                doSomeThing();
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeThing();
+            }
+        });
     }
 
     private void doSomeThing() {
-
+        animationDrawable1.start();
     }
 }
