3b9bdd22937ab19e3d8000176cf67b2f225e2857
WangJing <397383523@qq.com>
2020-07-16 10:43:51 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.wj.myapplication;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import io.reactivex.functions.Consumer;
 
 
 public class MainActivity extends BaseActivity {
 
-//    private Button button;
+    private Button button;
 
     @Override
     protected int setLayoutId() {
@@ -16,7 +26,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-//        button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
     }
 
     @Override
@@ -26,15 +36,29 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setEvent() {
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                doSomeThing();
-//            }
-//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeThing();
+            }
+        });
     }
 
     private void doSomeThing() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) {
+                        if (aBoolean) {
+                        } else {
+                            Toast.makeText(MainActivity.this, "-1", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void getFileBase64(String filepath){
 
     }
 }
