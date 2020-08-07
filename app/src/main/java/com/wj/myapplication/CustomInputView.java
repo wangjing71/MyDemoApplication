e4070251f4cc2747a0a03d2edd82d255ce52cabd
WangJing <397383523@qq.com>
2020-08-07 15:24:05 +0800

    Root检测 双开检测 模拟器检测

---
+++ app/src/main/java/com/wj/myapplication/CustomInputView.java
@@ -0,0 +1,66 @@
+package com.wj.myapplication;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * Description:
+ * Data：7/10/2018-10:18 AM
+ *
+ * @author yanzhiwen
+ */
+
+public class CustomInputView extends LinearLayout implements View.OnClickListener {
+
+    public CustomInputView(Context context) {
+        this(context, null);
+    }
+
+    public CustomInputView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomInputView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        inflate(context, R.layout.costomer_keyboard, this);
+        setItemClickListener(this);
+    }
+
+    /**
+     * 给每一个自定义数字键盘上的View 设置点击事件
+     *
+     * @param view
+     */
+    private void setItemClickListener(View view) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            int childCount = viewGroup.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                //不断的给里面所有的View设置setOnClickListener
+                View childView = ((ViewGroup) view).getChildAt(i);
+                setItemClickListener(childView);
+            }
+        } else {
+            view.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v instanceof TextView) {
+            String number = ((TextView) v).getText().toString().trim();
+            mPasswordEditText.addPassword(number);
+        }
+        if (v instanceof ImageView) {
+            mPasswordEditText.deletePassword();
+        }
+    }
+}
