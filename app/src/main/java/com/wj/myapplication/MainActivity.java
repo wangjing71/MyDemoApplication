234164d25fda343c9a5b8994615219a1564048ea
WangJing <397383523@qq.com>
2020-08-07 13:49:58 +0800

    Root检测 双开检测 模拟器检测

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.wj.myapplication;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends BaseActivity {
 
 //    private Button button;
+    private EditText edt1,edt2,edt3,edt4,edt5,edt6;
 
     @Override
     protected int setLayoutId() {
@@ -17,6 +21,12 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
 //        button = findViewById(R.id.button);
+        edt1 = findViewById(R.id.edt1);
+        edt2 = findViewById(R.id.edt2);
+        edt3 = findViewById(R.id.edt3);
+        edt4 = findViewById(R.id.edt4);
+        edt5 = findViewById(R.id.edt5);
+        edt6 = findViewById(R.id.edt6);
     }
 
     @Override
@@ -32,6 +42,24 @@ public class MainActivity extends BaseActivity {
 //                doSomeThing();
 //            }
 //        });
+        edt1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.toString().length() == 1){
+                    edt2.requestFocus();
+                }
+            }
+        });
     }
 
     private void doSomeThing() {
