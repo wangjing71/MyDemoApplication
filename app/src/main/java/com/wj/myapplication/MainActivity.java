42673c57f017ce7a3245b7fa75ca2122c176cd5c
Wangjing <1029821170@qq.com>
2021-02-19 12:48:12 +0800

    自定义键盘

--- app/src/main/java/com/wj/myapplication/MainActivity.java
+++ app/src/main/java/com/wj/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.xuexiang.xui.utils.StatusBarUtils;
 import com.xuexiang.xui.widget.guidview.FocusShape;
 import com.xuexiang.xui.widget.guidview.GuideCaseView;
 import com.xuexiang.xui.widget.guidview.OnViewInflateListener;
