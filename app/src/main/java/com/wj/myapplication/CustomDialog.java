265e968e36990c6debd6b6d37f904517b4513a13
Wangjing <1029821170@qq.com>
2021-02-24 16:32:01 +0800

    自定义键盘

---
+++ app/src/main/java/com/wj/myapplication/CustomDialog.java
@@ -0,0 +1,37 @@
+package com.wj.myapplication;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * author wangjing
+ * Date 2019/4/8
+ * Description
+ */
+public class CustomDialog extends Dialog {
+
+    public CustomDialog(@NonNull Context context) {
+        super(context);
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.dialog_layout);
+
+        Context context = getContext();
+        int divierId = context.getResources().getIdentifier("android:id/titleDivider", null, null);
+        View divider = findViewById(divierId);
+        if (divider != null) {
+            divider.setBackgroundColor(Color.TRANSPARENT);
+        }
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        show();
+        getWindow().setLayout(context.getResources().getDisplayMetrics().widthPixels * 4 / 5, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    }
+
+}
